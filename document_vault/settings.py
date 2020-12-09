5fa7abe5aaa8c4c7df6814d2940c1ec89042b6ae
Amrendra <amrendrasingh.bt@gmail.com>
2020-12-09 15:22:57 +0530

    Datatbase Setting

--- document_vault/settings.py
+++ document_vault/settings.py
@@ -94,21 +94,21 @@ WSGI_APPLICATION = 'document_vault.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
-DEBUG = config('DEBUG', default=True, cast=bool)
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+
+# DEBUG = config('DEBUG', default=True, cast=bool)
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 
 
