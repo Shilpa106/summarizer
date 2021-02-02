d9664ef2248335a16e83987b54d42f14aec47f3a
Amrendra <amrendrasingh.bt@gmail.com>
2021-02-02 16:21:10 +0530

    feature json changed

--- DocumentFeature/features.py
+++ DocumentFeature/features.py
@@ -1,10 +1,10 @@
-features = [
-            {"id": 1, "name": "Title Name"},
-            {"id": 2, "name": "Total no of Pages"},
-            {"id": 3, "name": "Total Word Count"},
-            {"id": 4, "name": "Total Images"},
-            {"id": 5, "name": "Content"},
-            {"id": 6, "name": "Total Paragraphs"},
-            {"id": 7, "name": "Lookup Word"},   
-            {"id": 8, "name": "Another One"}, 
-        ]
+[
+    {"id": 1, "name": "Title Name"},
+    {"id": 2, "name": "Total no of Pages"},
+    {"id": 3, "name": "Total Word Count"},
+    {"id": 4, "name": "Total Images"},
+    {"id": 5, "name": "Content"},
+    {"id": 6, "name": "Total Paragraphs"},
+    {"id": 7, "name": "Lookup Word"},   
+    {"id": 8, "name": "Another One"}, 
+]
